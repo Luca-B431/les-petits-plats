eabce702d64840bbeba4ca6eb7f05fd0cd0c9519
Luca Billat <lucabillatpro@gmail.com>
2025-01-11 16:04:45 +0100

    feat(header): add banner with search input and adjust styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      yellow: '#FFD15B',
+    },
+    extend: {
+      fontFamily: {
+        Manrope: ['Manrope', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
